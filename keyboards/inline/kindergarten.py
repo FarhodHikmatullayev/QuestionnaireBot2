8b7fa953bbf77af4045a62bb5b9e2a09a13c9bf4
Farhod <farhodjonhikmatullayev@gmail.com>
2024-11-29 19:08:44 +0500

    modified

--- keyboards/inline/kindergarten.py
+++ keyboards/inline/kindergarten.py
@@ -3,24 +3,30 @@ from gc import callbacks
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from aiogram.utils.callback_data import CallbackData
 
+from loader import db
+
 kindergarten_callback_query = CallbackData('kindergarten', 'number')
 
 
 async def kindergarten_inline_button():
     markup = InlineKeyboardMarkup()
     for i in range(1, 22):
-        text_button = f"{i} - MTM"
+        callback_data = f"{i} - MTM"
+        questionnaires = await db.select_questionnaires(kindergarten=callback_data)
+        text_button = f"{callback_data} ({len(questionnaires)})"
         markup.insert(
             InlineKeyboardButton(
                 text=text_button,
-                callback_data=kindergarten_callback_query.new(number=text_button)
+                callback_data=kindergarten_callback_query.new(number=callback_data)
             )
         )
-    text_button = "Betaraf/нейтрал"
+    callback_data = "Betaraf/нейтрал"
+    questionnaires = await db.select_questionnaires(kindergarten=callback_data)
+    text_button = f"{callback_data} ({len(questionnaires)})"
     markup.insert(
         InlineKeyboardButton(
             text=text_button,
-            callback_data=kindergarten_callback_query.new(number=text_button)
+            callback_data=kindergarten_callback_query.new(number=callback_data)
         )
     )
     return markup
